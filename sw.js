fa69ab2556595dbbd207b12d070dfe1c52c9f32b
muratanel <muratanel7@gmail.com>
2026-04-30 14:43:48 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'pos-cache-v1';
+// Указываем, какие файлы нужно скачать на телефон для оффлайн работы
+const urlsToCache = [
+    './pos2.html',
+    './manifest.json',
+    './icon.png'
+];
+
+// Установка: скачиваем файлы в кэш
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Перехват запросов: отдаем файлы из кэша, если нет интернета
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
