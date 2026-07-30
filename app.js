61ada389d15d6175856856e7edf694c3d551affc
muratanel <muratanel7@gmail.com>
2026-07-30 09:10:06 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -4500,7 +4500,7 @@ window.openQuickEditModal = function(id) {
 //     }
 // });
 // Открытие модального окна
-window.openModal = function(modalId) {
+window.openNewStyleModal = function(modalId) {
     const modal = document.getElementById(modalId);
     if (modal) {
         modal.style.display = 'flex';
